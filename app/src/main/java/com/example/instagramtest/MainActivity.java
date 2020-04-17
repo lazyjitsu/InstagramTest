46a5625baf8970b1cc3aaa9de879ba97439166bb
lazyjitsu <mperez58@hotmail.com>
2020-04-17 15:29:58 -0700

    Get Data from Parse Server

--- app/src/main/java/com/example/instagramtest/MainActivity.java
+++ app/src/main/java/com/example/instagramtest/MainActivity.java
@@ -10,14 +10,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText pPower,pSpeed,kPower,kSpeed,fName;
-    private Button btnSubmit;
+    private EditText pPower, pSpeed, kPower, kSpeed, fName;
+    private Button btnSubmit, btnGetAllMMA;
+    private TextView txtGetData;
+    private String allMMAFighters;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +38,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fName = findViewById(R.id.fName);
         btnSubmit = findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(MainActivity.this);
+        txtGetData = findViewById(R.id.txtGetData);
+        btnGetAllMMA = findViewById(R.id.btnGetAllMMA);
+
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MMA");
+                parseQuery.getInBackground("ZLqWDRoW07", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if (object != null && e == null) {
+                            txtGetData.setText(object.get("fName") + "" + " Punch Power: " + object.get("punchPower"));
+                        }
+                    }
+                });
+            }
+        });
+
+        btnGetAllMMA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allMMAFighters = "";
+                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("MMA");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null) {
+                            if (objects.size() > 0) { // IOW: we have at least 1 item
+                                // this for loop simply loops through the entire list of objects!
+                                for (ParseObject mmaFighter : objects) {
+                                    allMMAFighters = allMMAFighters + mmaFighter.get("fName") + ":" + mmaFighter.get("punchPower") + "\n";
+                                }
+                               // FancyToast.makeText(MainActivity.this, "Success Brother!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                               FancyToast.makeText(MainActivity.this, allMMAFighters, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                            } else {
+                                FancyToast.makeText(MainActivity.this, e + " is has an issue!", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                            }
+                        }
+                    }
+
+                });
+            }
+        });
 
 
-
-    }
 //   public void helloWorldTapped(View view) {
 //        ParseObject boxer = new ParseObject("Boxer");
 //        boxer.put("punchspeed", 200);
@@ -68,37 +119,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //
 //    }
 
-    @Override
-    public void onClick(View v) {
 
-        // Log.i("TST",pPower.getText() + " heu diude!");
-        try {
-            final ParseObject fighter = new ParseObject("Fighter");
-            fighter.put("fName", fName.getText().toString());
-            fighter.put("punchSpeed", Integer.parseInt(pSpeed.getText().toString()));
-            fighter.put("punchPower", Integer.parseInt(pPower.getText().toString()));
-            fighter.put("kickPower", Integer.parseInt(kSpeed.getText().toString()));
-            fighter.put("kickSpeed", Integer.parseInt(kPower.getText().toString()));
-
-            fighter.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-
-                    if (e == null) {
-                        FancyToast.makeText(MainActivity.this, fighter.get("fName") + " is saved to Server!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                        //Toast.makeText(MainActivity.this,fighter.get("firstName") + " saved to Server!",Toast.LENGTH_LONG).show();
-                    } else {
-                        FancyToast.makeText(MainActivity.this, fighter.get("fName") + " is saved to Server!", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-                        // Toast.makeText(MainActivity.this,e.getMessage() + " os your error!",Toast.LENGTH_LONG).show();
-                        //Log.i("Instagram",e + " Is the ERRor!!");
-                    }
-                }
-            });
-        } catch (Exception e) {
-            FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-
-        }
-    }
 
 //    public void submitUserData(View v) {
 //        Log.i("TST",fName.getText() + "  is the text");
@@ -119,4 +140,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //
 //        }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        try {
+            final ParseObject fighter = new ParseObject("MMA");
+            fighter.put("fName", fName.getText().toString());
+            fighter.put("punchSpeed", Integer.parseInt(pSpeed.getText().toString()));
+            fighter.put("punchPower", Integer.parseInt(pPower.getText().toString()));
+            fighter.put("kickPower", Integer.parseInt(kSpeed.getText().toString()));
+            fighter.put("kickSpeed", Integer.parseInt(kPower.getText().toString()));
+
+            fighter.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+
+                    if (e == null) {
+                        FancyToast.makeText(MainActivity.this, fighter.get("fName") + " is saved to Server!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                        //Toast.makeText(MainActivity.this,fighter.get("firstName") + " saved to Server!",Toast.LENGTH_LONG).show();
+                    } else {
+                        FancyToast.makeText(MainActivity.this, e + " is has an issue!", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                        // Toast.makeText(MainActivity.this,e.getMessage() + " os your error!",Toast.LENGTH_LONG).show();
+                        //Log.i("Instagram",e + " Is the ERRor!!");
+                    }
+                }
+            });
+        } catch (Exception e) {
+            FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+        }
+    }
 }
